d6d41e6a54bc495a4f4c899284dcdb90b28c3b24
annaSkr45 <skripnikana836@gmail.com>
2025-06-18 21:50:45 +0300

    Initial project structure: backend, frontend, Docker, tests

--- backend/app.py
+++ backend/app.py
@@ -9,15 +9,29 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 app = FastAPI()
 
-# Додамо CORS (поки що для localhost)
+origins = [
+    "http://localhost:8001",
+    "http://127.0.0.1:8001",
+]
+
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=["http://localhost:8001"],
     allow_origins=["*"],
+    # allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+fake_users_db = {
+    STUDENT_ID: {
+        "username": STUDENT_ID,
+        "full_name": STUDENT_ID,
+        "hashed_password": "password123",  # нерекомендовано зберігати так на проді
+        "disabled": False,
+    }
+}
 
 # Пам'ять для збереження джерел (для кожного STUDENT_ID окремо)
 store = {STUDENT_ID: SOURCES.copy()}
